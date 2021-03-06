c7466dc6e89b6accbb8c54b112bdb63d00e13565
hedeesa <saeedeh.moghimi@cafebazaar.ir>
2021-03-07 01:12:42 +0330

    Add Webserver to serve code

--- main.go
+++ main.go
@@ -1,36 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"errors"
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	_ "ncode/docs"
 	"ncode/ncode"
+	"fmt"
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
+var router *gin.Engine
 
-func main(){
+// @title NationalCode
+// @version 0.1
+// @description This API validates Iran's NationalCode
 
-	userNationalID, error := getArgument()
-	if (error != nil){
-		os.Exit(1)
-	}
+// @contact.name Saeede Moghimi
+// @contact.url saeedehmoghimi.com
+// @contact.email moghimi.saeedeh@gmail.com
 
-	nID := ncode.NewNationalID(string(userNationalID))
-	if nID.Validity == true{
-		fmt.Println("This NationaID is Valid")
-	} else{
-		fmt.Println("This NationaID is Invalid")
-	}
+// @host 127.0.0.1:8080
+// @BasePath /validation/
+
+
+func main() {
+
+  router = gin.Default()
+  router.Use(cors.Default())
+  url := ginSwagger.URL("127.0.0.1:8080/swagger/doc.json")
+  router.GET("/swagger/*any",ginSwagger.WrapHandler(swaggerFiles.Handler,url))
+
+  
+  router.GET("/validation/", validateNationalCode)
+
+
+  router.Run()
 
 }
 
 
-func getArgument() (string,error){
-	input := os.Args[1:]
-	if len(input) != 0 {
-		return input[0],nil
-	}else{
-		fmt.Println("You must enter nationalID like this: \"go run main.go NATIONALID\"")
-		return "",errors.New("invalid")
-	}
+// validateNationalCode godoc
+// @Summary Validates Iranian's NationalCode
+// @Description get NationalCode by id
+// @Tags NationalCode
+// @Accept  text/plain
+// @Produce  json
+// @Param  id query string true "NationalCode ID"
+// @Router / [get]
+// @Success 200 {object} ncode.NationalCode
+func validateNationalCode(c *gin.Context){
+	code := c.Query("id")
+
+	fmt.Println(code)
+	nationalCode := ncode.NewNationalCode(code)
+	c.JSON(200,nationalCode)
+
 }
